7326377bc707740b72ff584a883cff64d24340c6
charcodes24 <charcodes24@gmail.com>
2021-09-16 14:07:35 -0700

    Added form to login page with handleInput function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,3 +1,42 @@
+import { useState } from "react"
+
 export default function Login() {
-    return <h1>Login</h1>
+    const [form, setForm] = useState({
+        username: "",
+        password: ""
+    })
+
+
+    function handleInput(e) {
+      setForm({
+        ...form,
+        [e.target.name]: e.target.value,
+      });
+    }
+
+    function handleSubmit() {
+        console.log('hi')
+    }
+
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleInput}
+            type="text"
+            name="username"
+            value={form.username}
+            placeholder="username"
+          />
+          <input
+            onChange={handleInput}
+            type="text"
+            name="password"
+            value={form.password}
+            placeholder="password"
+          />
+          <button>Log-In!</button>
+        </form>
+      </div>
+    );
 }
