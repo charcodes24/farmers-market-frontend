909f5761eff5f96801811ce9360b7c576721f5b9
charcodes24 <charcodes24@gmail.com>
2021-11-03 15:18:30 -0700

    Implemented useForm in VnedorSignup Component

--- src/components/VendorSignup.js
+++ src/components/VendorSignup.js
@@ -1,121 +1,65 @@
-import { useState, useEffect } from "react"
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
 
-import { createVendor, clearErrors } from "../features/login/loginSlice";
+import { createVendor } from "../features/login/loginSlice";
 
 
 export default function VendorSignup() {
-  const dispatch = useDispatch()
-  const hasError = useSelector(state => state.login.hasError)
-  const errors = useSelector(state => state.login.errors)
-    const [form, setForm] = useState({
-      name: "",
-      description: "",
-      email: "",
-      username: "",
-      password: "",
-      password_confirmation: "",
-      is_vendor: true,
-    });
-  
-  console.log(errors.length)
+  //USEFORM HOOK
+  const {
+    register,
+    handleSubmit,
+    setFocus,
+    formState: { errors },
+  } = useForm();
 
-    function handleInput(e) {
-      if (e.target.name === "name") {
-        setForm({
-          ...form,
-          [e.target.name]: e.target.value
-            .split(" ")
-            .map(
-              (word) =>
-                word.charAt(0).toUpperCase() + word.substring(1).toLowerCase()
-            )
-            .join(" "),
-        });
-      } else {
-        setForm({
-          ...form,
-          [e.target.name]: e.target.value,
-        });
-      }
-    }
-  
+  //REDUX
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    return function () {
-      dispatch(clearErrors())
-    }
-  }, [dispatch]);
+    setFocus("name")
+  }, [setFocus]);
 
-    function handleSubmit(e) {
-      e.preventDefault()
-        dispatch(createVendor(form))
-    }
+  function submitCreateVendor(data) {
+    dispatch(createVendor(data));
+  }
 
-
-    return (
-      <div>
-        <div>
-          <form className="signup-vendor" onSubmit={handleSubmit}>
-            <input
-              onChange={handleInput}
-              type="text"
-              name="name"
-              value={form.name}
-              placeholder="name"
-            />
-            <textarea
-              onChange={handleInput}
-              type="text"
-              name="description"
-              value={form.description}
-              placeholder="Shop description..."
-            />
-            <input
-              onChange={handleInput}
-              type="text"
-              name="email"
-              value={form.email}
-              placeholder="email"
-            />
-            <input
-              onChange={handleInput}
-              type="text"
-              name="username"
-              value={form.username}
-              placeholder="username"
-            />
-            <input
-              onChange={handleInput}
-              type="password"
-              name="password"
-              value={form.password}
-              placeholder="password"
-            />
-            <input
-              onChange={handleInput}
-              type="password"
-              name="password_confirmation"
-              value={form.password_confirmation}
-              placeholder="password confirmation"
-            />
-            <button type="submit" className="btn signup">
-              Sign Up!
-            </button>
-            <p className="link">
-              Are you a customer? Sign-up{" "}
-              <Link to="/signup_customer">here.</Link>
-            </p>
-          </form>
-        </div>
-        {hasError ? (
-          <div className="alert">
-            <h3>Please fix the following:</h3>
-            {errors.map((error) => (
-              <p>{error}</p>
-            ))}
-          </div>
-        ) : null}
-      </div>
-    );
+  return (
+    <div>
+      <form className="signup-vendor" onSubmit={handleSubmit(submitCreateVendor)}>
+        <input {...register("name", { required: true })} placeholder="name" />
+        {errors.name?.type === "required" && "*required"}
+        <textarea
+          {...register("description", { required: true })}
+          placeholder="Shop description..."
+        />
+        {errors.description?.type === "required" && "*required"}
+        <input {...register("email", { required: true })} placeholder="email" />
+        {errors.email?.type === "required" && "*required"}
+        <input
+          {...register("username", { required: true })}
+          placeholder="username"
+        />
+        {errors.username?.type === "required" && "*required"}
+        <input
+          {...register("password", { required: true })}
+          placeholder="password"
+        />
+        {errors.password?.type === "required" && "*required"}
+        <input
+          {...register("passwordConfirmation", { required: true })}
+          placeholder="password confirmation"
+        />
+        {errors.passwordConfirmation?.type === "required" && "*required"}
+        <button type="submit" className="btn signup">
+          Sign Up!
+        </button>
+        <p className="link">
+          Are you a customer? Sign-up <Link to="/signup_customer">here.</Link>
+        </p>
+      </form>
+    </div>
+  );
 }
