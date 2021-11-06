0ef953910a796f88019ffd0e6c405ef87340b5ff
charcodes24 <charcodes24@gmail.com>
2021-11-05 19:34:50 -0700

    Removed animations and clip-path for Header Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,6 @@ export default function Header() {
           <div className="text-box">
             <h1 className="heading-primary">
               <span className="heading-primairy-main">Welcome to the Farmer's Market!</span>
-              <span className="headering-primary-sub">A social-distancing, shopping alternative...</span>
             </h1>
           </div>
         </header>
