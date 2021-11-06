cf25b14b905fabb406266571e5ce575b2dd4c0b5
charcodes24 <charcodes24@gmail.com>
2021-11-05 19:44:42 -0700

    Reduced height of Header and removed clip-path shape, fixed off-set text

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,8 @@ export default function Header() {
         <header className="header">
           <div className="text-box">
             <h1 className="heading-primary">
-              <span className="heading-primairy-main">Welcome to the Farmer's Market!</span>
+              <span className="heading-primary-main">Welcome to the Farmer's Market</span>
+              <span className="heading-primary-sub">Happy Shopping!</span>
             </h1>
           </div>
         </header>
