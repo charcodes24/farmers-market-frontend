5d9d15069cd6c08340678cdb4099a7ca7d29c169
charcodes24 <charcodes24@gmail.com>
2021-11-06 21:24:52 -0700

    Fixed back of card styling so description does not overflow

--- src/components/VendorCard.js
+++ src/components/VendorCard.js
@@ -3,19 +3,17 @@ import { Link } from "react-router-dom"
 import "./VendorCard.css";
 
 export default function VendorCard({ vendor }) {
-  const { id, name } = vendor
+  const { id, name, description } = vendor
 
   return (
     <div className="flip-card">
       <Link to={`/vendors/${id}`}>
       <div className="flip-card-inner">
         <div className="flip-card-front">
-          <Link to={`/vendors/${id}`} className="card-link">
-            <p>{name}</p>
-          </Link>
+            {name}
         </div>
         <div className="flip-card-back">
-          <h1>{name}</h1>
+          <p className="card-description">{description}</p>
         </div>
       </div>
       </Link>
